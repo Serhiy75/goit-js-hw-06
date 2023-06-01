9b6421b759aea29b9a6d2768e639d0e2cb669d03
sstfnk3107 <s.stefanyk75@gmail.com>
2023-06-01 23:28:11 +0300

    first task done

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,8 @@
 const listId = document.querySelector('ul#categories');
 console.log(`Number of categories: ${listId.childElementCount}`);
+
+const itemClass = document.querySelectorAll('.item');
+itemClass.forEach(itemOfList => {
+    console.log(`Category: ${itemOfList.firstElementChild.textContent}`);
+    console.log(`Elements: ${itemOfList.lastElementChild.childElementCount}`);
+});
