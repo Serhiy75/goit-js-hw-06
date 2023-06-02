031fe10f5f01c84afa6d1a13f43b410c700f3e27
sstfnk3107 <s.stefanyk75@gmail.com>
2023-06-02 14:10:14 +0300

    six task done

---
+++ js/task-06.js
@@ -0,0 +1,17 @@
+const input = document.querySelector('#validation-input');
+
+input.addEventListener('blur', e => {
+    const length = input.getAttribute('data-length');
+    const valueLength = e.target.value.length;
+
+    if (valueLength === Number(length)) {
+        switcher('valid', 'invalid');
+    } else {
+        switcher('invalid', 'valid');
+    }
+});
+
+function switcher(add, remove) {
+    input.classList.add(add);
+    input.classList.remove(remove);
+};
