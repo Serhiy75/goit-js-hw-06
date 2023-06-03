4092809a7f492055ea20104b6565eb6acbf7693b
sstfnk3107 <s.stefanyk75@gmail.com>
2023-06-03 11:14:40 +0300

    nineth task done

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,34 @@
+const body = document.querySelector('body');
+const span = document.querySelector('.color');
+const btn = document.querySelector('.change-color');
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+btn.addEventListener('click', () => {
+  const hexColor = getRandomHexColor();
+  body.style.backgroundColor = hexColor;
+  span.textContent = hexColor;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
